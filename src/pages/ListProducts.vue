155c161928092429803d0544cd8d4f73cab87e54
maloveroyatno <mam229922@mail.ru>
2024-06-24 19:26:32 +0300

    make a beauty page of products

--- src/pages/ListProducts.vue
+++ src/pages/ListProducts.vue
@@ -3,8 +3,20 @@
         <v-container class="navigation">
             <Navigation />
         </v-container>
-        <v-container class="list_cards">
-            <v-cointainer class="card" v-for="(product, index) in slicedProducts" :key="index">
+        <v-container class="search">
+            <v-text-field label="Поиск" variant="underlined" v-model="search" ></v-text-field>
+        </v-container>
+        <v-container v-if="slicedProducts.length > 0" class="list_cards">
+            <v-cointainer class="card_info">
+                <span class="info_name">Название <span class="pfc">КБЖУ(на 100 г.): </span></span>
+                <v-container class="info">
+                    <span class="pfc">Калории</span>
+                    <span class="pfc">Белки</span>
+                    <span class="pfc">Жиры</span>
+                    <span class="pfc">Углеводы</span>
+                </v-container>  
+            </v-cointainer>
+            <v-cointainer class="card" v-for="(product, index) in slicedProducts" :key="index" >
                 <h1 class="name"> {{ product.name }}</h1>
                 <v-container class="info">
                     <span class="pfc">{{ product.calories }}</span>
@@ -13,7 +25,7 @@
                     <span class="pfc">{{ product.carbs }}</span>
                 </v-container>
             </v-cointainer>
-            <h1><button @click="showLess"> < </button> {{ currentPage }} <button @click="showMore"> > </button></h1>
+            <v-pagination :length="Math.ceil(filteredProducts.length / pageSize)" v-model="currentPage" total-visible="8" @click="sliceProducts" > </v-pagination>
         </v-container>
     </v-container>
 </template>
@@ -27,23 +39,27 @@ const appStore = useAppStore();
 const pageSize = 8;
 const currentPage = ref(1);
 const slicedProducts = ref([]);
+const search = ref('');
 
-const showMore = () => {
-    currentPage.value++;
-    sliceProducts();
+const filteredProducts = computed(() => {
+if (search.value.trim() === '') {
+    return appStore.productsAll;
 }
-const showLess = () => {
-    if (currentPage.value === 1) { return; }
-    currentPage.value--;
-    sliceProducts();
-}
+const lowerCaseSearch = search.value.toLowerCase();
+return appStore.productsAll.filter((product) => {
+    const lowerCaseName = product.name.toLowerCase();
+    return lowerCaseName.includes(lowerCaseSearch);
+});
+});
 
 const sliceProducts = () => {
     const start = (currentPage.value - 1) * pageSize;
     const end = start + pageSize;
-    slicedProducts.value = appStore.productsAll.slice(start, end);
+    slicedProducts.value = filteredProducts.value.slice(start, end);
 }
-
+watch(search, (newValue) => {
+    sliceProducts();
+});
 onMounted(() => {
     sliceProducts();
 });
@@ -64,6 +80,7 @@ onMounted(() => {
     width: 100%;
 }
 .list_cards {
+    margin-top: 0px;
     flex-wrap: wrap;
     display: flex;
     flex-direction:column;
@@ -88,13 +105,30 @@ onMounted(() => {
     justify-content: space-between;
     width: 100%;
 }
+.card_info{
+    display: flex;
+    padding: 10px;
+    justify-content: space-between;
+    flex-direction: row;
+    font-size: 12px;
+    margin-top: 5px;
+    margin-bottom: 5px;
+    width: 100%;
+    margin-right: 25px;
+}
 .info{
     padding: 0px;
     display: flex;
     flex-direction: row;
-    align-items: right;
     justify-content:right;
     width: 50%;
+}
+.info_pfc{
+    padding: 0px;
+    display: flex;
+    flex-direction: row;
+    justify-content:center;
+    width: 10%;
 }
 .name{
     width: 40%;
@@ -105,12 +139,35 @@ onMounted(() => {
     font-style: normal;
     margin-left: 25px;
 }
+.info_name{
+    width: 40%;
+    font-size: 20px;
+    font-family: "Yanone Kaffeesatz", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 300;
+    font-style: normal;
+    margin-left: 25px;
+    justify-content: space-between;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    text-wrap:nowrap;
+}
 .span{
     margin-left: 25px;
     margin-right: 25px;
 }
 .pfc{
+    font-size: 20px;
+    font-family: "Yanone Kaffeesatz", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 300;
+    font-style: normal;
     width: 25%;
     margin-right: 15px;
+}
+.search{
+    width: 85%;
+    padding-bottom: 0px;
 }
 </style>
